af78d6bbce0b9908f455815210cc411e1705a20d
Bangun Edma <bangun.edma.saputra@gmail.com>
2021-02-21 16:57:49 +0700

    Initial Commit for API Profject

---
+++ uwati-api/src/main/kotlin/io/github/edmaputra/uwatiapi/UwatiApiApplication.kt
@@ -0,0 +1,11 @@
+package io.github.edmaputra.uwatiapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UwatiApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<UwatiApiApplication>(*args)
+}
---
+++ uwati-api/src/test/kotlin/io/github/edmaputra/uwatiapi/UwatiApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.edmaputra.uwatiapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UwatiApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
