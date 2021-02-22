2c238be42049339aad0e962ce98a98192b96d78e
Bangun Saputra <bangun.edma.saputra@gmail.com>
2021-02-22 22:58:29 +0700

    Set first configuration repository.

--- uwati-api/config-service/src/main/kotlin/io/github/edmaputra/uwati/configservice/ConfigServiceApplication.kt
+++ uwati-api/config-service/src/main/kotlin/io/github/edmaputra/uwati/configservice/ConfigServiceApplication.kt
@@ -2,8 +2,10 @@ package io.github.edmaputra.uwati.configservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class ConfigServiceApplication
 
 fun main(args: Array<String>) {
