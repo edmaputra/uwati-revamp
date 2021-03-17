ed06b00690c45dd8822d0159e0d8f9783e84790d
Bangun Saputra <bangun.edma.saputra@gmail.com>
2021-03-17 21:15:06 +0700

    feat: Add response POJO

---
+++ uwati-api/profile/src/main/kotlin/io/github/edmaputra/uwati/profile/web/PageResponse.kt
@@ -0,0 +1,12 @@
+package io.github.edmaputra.uwati.profile.web
+
+data class PageResponse<T>(
+
+    val content: T,
+
+    val pageNumber: Int,
+
+    val totalPage: Int,
+
+    val totalItems: Int
+)
