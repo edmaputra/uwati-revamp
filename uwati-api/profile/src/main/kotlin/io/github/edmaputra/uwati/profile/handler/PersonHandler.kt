c1ee7fa65158c25153da76d40602a2b860e8bb13
Bangun Edma <bangun.edma.saputra@gmail.com>
2021-03-21 12:31:00 +0700

    feat: Add Person router handler.

--- uwati-api/profile/src/main/kotlin/io/github/edmaputra/uwati/profile/handler/PersonHandler.kt
+++ uwati-api/profile/src/main/kotlin/io/github/edmaputra/uwati/profile/handler/PersonHandler.kt
@@ -2,6 +2,8 @@ package io.github.edmaputra.uwati.profile.handler
 
 import io.github.edmaputra.uwati.profile.entity.Person
 import io.github.edmaputra.uwati.profile.service.PersonService
+import io.github.edmaputra.uwati.profile.web.request.PersonCreateRequest
+import io.github.edmaputra.uwati.profile.web.request.PersonUpdateRequest
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
@@ -10,13 +12,41 @@ import reactor.core.publisher.Mono
 
 @Component
 class PersonHandler(
-    private val service: PersonService
+  private val service: PersonService
 ) {
 
-    fun listPersons(request: ServerRequest): Mono<ServerResponse> {
-        return ServerResponse
-            .ok()
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(service.listPersons(), Person::class.java)
-    }
+  fun getAll(request: ServerRequest): Mono<ServerResponse> {
+    return ServerResponse
+      .ok()
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(service.getAll(), Person::class.java)
+  }
+
+  fun getById(request: ServerRequest): Mono<ServerResponse> {
+    return ServerResponse
+      .ok()
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(service.getById(Mono.just(request.pathVariable("id"))), Person::class.java)
+  }
+
+  fun create(request: ServerRequest): Mono<ServerResponse> {
+    val result = service.create(request.bodyToMono(PersonCreateRequest::class.java))
+    return ServerResponse.created(request.uri())
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(result, Person::class.java)
+  }
+
+  fun update(request: ServerRequest): Mono<ServerResponse> {
+    val result = service.update(request.bodyToMono(PersonUpdateRequest::class.java))
+    return ServerResponse.ok()
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(result, Person::class.java)
+  }
+
+  fun delete(request: ServerRequest): Mono<ServerResponse> {
+    val result = service.delete(Mono.just(request.pathVariable("id")))
+    return ServerResponse.ok()
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(result, Person::class.java)
+  }
 }
