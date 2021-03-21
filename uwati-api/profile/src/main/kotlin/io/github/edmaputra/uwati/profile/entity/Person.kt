52f43adad777978cce45bdf97cb4be329b22a34f
Bangun Edma <bangun.edma.saputra@gmail.com>
2021-03-21 12:37:04 +0700

    Clean code.

--- uwati-api/profile/src/main/kotlin/io/github/edmaputra/uwati/profile/entity/Person.kt
+++ uwati-api/profile/src/main/kotlin/io/github/edmaputra/uwati/profile/entity/Person.kt
@@ -4,20 +4,20 @@ import io.github.edmaputra.uwati.profile.enum.PersonType
 import org.springframework.data.annotation.Id
 
 data class Person(
-    @Id
-    val id: String,
+  @Id
+  val id: String,
 
-    val personId: String,
+  val personId: String,
 
-    val name: String,
+  val name: String,
 
-    val email: String,
+  val email: String,
 
-    val type: PersonType,
+  val type: PersonType,
 
-    val address: Map<String, Any>,
+  val address: Map<String, Any>,
 
-    val phone: String,
+  val phone: String,
 
-    val metadata: Map<String, Any>
+  val metadata: Map<String, Any>
 )
