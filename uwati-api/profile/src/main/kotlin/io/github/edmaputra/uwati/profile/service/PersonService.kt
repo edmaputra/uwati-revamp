a8f1f8e6039dfe0a0ca21f2edadddc1ec4e9eb49
Bangun Edma <bangun.edma.saputra@gmail.com>
2021-03-21 12:30:39 +0700

    feat: Add Person interface and create mock implementation

--- uwati-api/profile/src/main/kotlin/io/github/edmaputra/uwati/profile/service/PersonService.kt
+++ uwati-api/profile/src/main/kotlin/io/github/edmaputra/uwati/profile/service/PersonService.kt
@@ -1,10 +1,21 @@
 package io.github.edmaputra.uwati.profile.service
 
 import io.github.edmaputra.uwati.profile.entity.Person
+import io.github.edmaputra.uwati.profile.web.request.PersonCreateRequest
+import io.github.edmaputra.uwati.profile.web.request.PersonUpdateRequest
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 interface PersonService {
 
-    fun listPersons(): Flux<Person>
+  fun getAll(): Flux<Person>
+
+  fun getById(request: Mono<String>): Mono<Person>
+
+  fun create(request: Mono<PersonCreateRequest>): Mono<Person>
+
+  fun update(request: Mono<PersonUpdateRequest>): Mono<Person>
+
+  fun delete(request: Mono<String>): Mono<Person>
 
 }
